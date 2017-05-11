2d3ebabe518a210aefb90f9fecb6ca3178d55853
Mauricio Leal <maltron@gmail.com>
2017-05-11 20:13:32 -0300

    First Commit

---
+++ src/main/java/com/maltron/exemplohackaton/RESTEnabled.java
@@ -0,0 +1,8 @@
+package com.maltron.exemplohackaton;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class RESTEnabled extends Application {
+}
---
+++ src/main/java/com/maltron/exemplohackaton/Recurso.java
@@ -0,0 +1,18 @@
+package com.maltron.exemplohackaton;
+
+import java.io.Serializable;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/exemplo")
+public class Recurso implements Serializable {
+    
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response meuNome() {
+        return Response.ok("Mauricio").build();
+    }
+}
